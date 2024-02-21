16c3dfd13479f5261d86f0d9795764568ca93a9f
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2024-02-21 13:54:55 -0300

    feat: Add users & basic http auth

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from models import Pessoas
+from flask_httpauth import HTTPBasicAuth
+from models import Pessoas, Usuarios
 
+auth = HTTPBasicAuth()
 app = Flask(__name__)
 api = Api(app)
 
+#IMPLEMENT PASSWD HASH TO FINISH
+@auth.verify_password
+def verificacao(login, senha):
+    if not (login, senha):
+        return False
+    return Usuarios.query.filter_by(login=login, senha=senha).first()
 class Pessoa(Resource):
 
     def get(self, nome):
@@ -49,6 +57,34 @@ class ListaPessoas(Resource):
             'idade':pessoa.idade
         }
         return response
+    
+class Usuario(Resource):
+    @auth.login_required
+    def get(self,username):
+            usuario = Usuarios.query.filter_by(login=username).first()
+            #usuario = Usuarios.query.all()
+            try:
+                response = {
+                    'login':usuario.login,
+                    'id':usuario.id 
+                }
+            except AttributeError:
+                print(usuario)
+                response = {
+                    'status':'error',
+                    'mensagem':'Usuário nao encontrado'
+                }
+            return response
+    
+    def post(self, username):
+            dados = request.json
+            usuario = Usuarios(login=dados['login'], senha=dados['senha'])
+            usuario.save()
+            response = {
+                'id':usuario.id,
+                'login':usuario.login
+            }
+            return response
 class Home(Resource):
 
     def get(self):
@@ -57,6 +93,7 @@ class Home(Resource):
 api.add_resource(Home, '/')
 api.add_resource(Pessoa, '/pessoa/<string:nome>/')
 api.add_resource(ListaPessoas, '/pessoa/')
+api.add_resource(Usuario, '/usuario/<string:username>/')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -28,7 +28,22 @@ class Pessoas(Base):
         db_session.delete(self)
         db_session.commit()
 
+class Usuarios(Base):
+    __tablename__='usuarios'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    login = Column(String(20), unique=True)
+    senha = Column(String(20))
 
+    def __repr__(self):
+        return '<Usuario {}>'.format(self.login)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
 
 def init_db():
     Base.metadata.create_all(bind=engine)
